6ea196a55116571ca4ec427a1fa41e2d3a63a122
oxshipon1@gmail.com <oxshipon1@gmail.com>
2026-05-13 13:51:14 +0600

    solved

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port  ${PORT}`));
   } catch (err) {
     console.error(err);
     process.exit(1);
